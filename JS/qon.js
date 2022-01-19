fe9c4f5ff6d24482ca31f40f36ca08a18f1ebed1
CYIMANA-Faisal <cyimanafaisal@gmail.com>
2022-01-19 11:54:02 +0200

    changed the design

--- JS/qon.js
+++ JS/qon.js
@@ -1,16 +1,20 @@
-const contactForm = document.getElementById("contactForm");
+const contactForm = document.getElementById("contact-form");
 contactForm.addEventListener("submit", (e) => {
     e.preventDefault();
     // Get the values from the form
     const name = e.target.elements.name.value
     const message = e.target.elements.text.value
     const email = e.target.elements.email.value
+    const subject = e.target.elements.subject.value
+    const phonenumber = e.target.elements.phonenumber.value
     // defining the message
     const query = {
         id: uuidv4(),
         name: name,
         email: email,
         message: message,
+        subject: subject,
+        phonenumber: phonenumber,
         sentOn: Date.now()
     }
     // Let first get the existing queries
@@ -19,4 +23,9 @@ contactForm.addEventListener("submit", (e) => {
     // Let send the message to the local storage
     localStorage.setItem("queries", JSON.stringify(queries))
     window.alert("Thanak you for contacting me")
+    e.target.elements.name.value = ''
+    e.target.elements.text.value = ''
+    e.target.elements.email.value = ''
+    e.target.elements.subject.value = ''
+    e.target.elements.phonenumber.value = ''
 })
